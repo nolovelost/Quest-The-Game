23429d76da8ccbd70d61231f63b61fe99e2f591d
Pratyush B Singh <prats.bdr@gmail.com>
2017-11-16 14:55:13 +0000

    spell timer script

---
+++ MageOfficial/Assets/Scripts/PlayerStatus/TimerSlider/TimerBehaviour.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerBehaviour : MonoBehaviour
+{
+    private float TotalTime = 3.0f;
+    private float CurrentTime;
+    public float StartingTimer = 100.0f;                            // The amount of Time the player starts with
+    public float CurrentTimer;                                      // The current Time the player has.
+    public Slider TimeSlider;                                       // Reference to the UI's Time bar.
+    public Slider TimeSlider2;
+    public Image TimerEndingImage;                                  // Reference to an image to flash on the screen on being hurt.
+    public float flashSpeed = 5f;                                   // The speed the TimerEndingImage will fade at.
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);         // The colour the TimerEndingImage is set to, to flash.
+    bool IsTiming = false;
+    float NormalizedTime;
+    
+    // Use this for initialization
+    void Start ()
+    {
+        CurrentTimer = StartingTimer;
+        CurrentTime = TotalTime;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        TimeSlider.value = CurrentTimer;
+        TimeSlider2.value = CurrentTimer;
+
+        if (IsTiming == true)
+        {
+            CurrentTime = CurrentTime - Time.deltaTime;
+            NormalizedTime = CurrentTime / TotalTime;
+            CurrentTimer = NormalizedTime * 100.0f;
+        }
+
+        if (Input.GetButtonDown("ToggleInventory"))
+        {
+            SetTimer(3.0f);
+            StartTimer();
+        }
+
+        if (CurrentTime <= 0.0f)
+        {
+            ResetTimer();
+        }
+	}
+
+    public void SetTimer(float Seconds)
+    {
+        TotalTime = Seconds;
+    }
+
+    public void StartTimer()
+    {
+        IsTiming = true;
+    }
+
+    public void ResetTimer()
+    {
+        CurrentTime = TotalTime;
+        IsTiming = false;
+    }
+}
