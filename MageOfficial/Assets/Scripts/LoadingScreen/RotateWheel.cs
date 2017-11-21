20c2b5885bd850534dcd18df453705a8eee1865f
Pratyush B Singh <prats.bdr@gmail.com>
2017-11-21 10:22:55 +0000

    loading screen scripts

---
+++ MageOfficial/Assets/Scripts/LoadingScreen/RotateWheel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWheel : MonoBehaviour {
+
+    Quaternion CurrentRotation;
+    public float speed = 100.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //CurrentRotation = gameObject.GetComponent<RectTransform>().rotation();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //CurrentRotation = gameObject.GetComponent<RectTransform>().rotation();
+        //CurrentRotation = Quaternion.eulerAngles(CurrentRotation.euler.x, CurrentRotation.euler.y, CurrentRotation.euler.z + (speed * Time.deltaTime));
+
+        gameObject.GetComponent<RectTransform>().transform.Rotate(0, 0, speed * Time.deltaTime);
+    }
+}
